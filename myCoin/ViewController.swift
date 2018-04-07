4e7003419b2f3c6574cad9e1f0e0edbf8ba741db
Artem Grebenkin <artemgrebenkin@Mac-Admin.local>
2018-04-07 20:49:30 +0600

    Initial Commit

---
+++ myCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myCoin
+//
+//  Created by Artem Grebenkin on 4/7/18.
+//  Copyright © 2018 Artem Grebenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
